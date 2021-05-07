07d0a4ba9155486b0dce9200d35a9e6937596b76
ahmedabouhamed <45257599+ahmedabuhamid@users.noreply.github.com>
2021-05-07 02:02:06 +0200

    Report which outlook interface being used
    Use this script as part of an extension attribute in Jamf to report if the currently logged-in user is using the new outlook interface or using the classic interface?

---
+++ outlook_interface.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+: <<ABOUT_THIS_SCRIPT
+	Written by: Ahmed Abouhamed
+	eng.abuhamed@gmail.com
+	Last updated: May 6, 2021
+	Originally posted: May 6, 2021
+	Purpose: Use this script as part of an extension attribute in Jamf
+	to report if the current loggedin user is using the new outlook
+	interface or using the classic interface ?
+	LICENSE: Except where otherwise noted, this work is licensed under
+	http://creativecommons.org/licenses/by/4.0/
+	
+	INSTRUCTIONS
+
+	1) Log in to the Jamf Pro server.
+	2) Navigate to JSS Settings > Computer Management > Extension Attributes.
+	3) Click the " + " button to create a new extension attribute with these settings:
+	   Display Name: Outlook interface
+	   Description: Reports the used outlook interface used (New/Classic).
+	   Data Type: String
+	   Inventory Display: Extension Attributes
+	   Input Type: Script
+	   Script: < Copy and paste this entire script >
+	4) Save the extension attribute.
+	5) Use Recon.app or "sudo jamf recon" to inventory a Mac with Office installed.
+	6) Result can be viewed individually for each computer or create a smart group with the below criteria:
+			a) 'Outlook interface' [LIKE] 'New Outlook'  >> to group all computers with New Outlook
+			b) 'Outlook interface' [LIKE] 'Classic Outlook'  >> to group all computers with Classic Outlook
+ABOUT_THIS_SCRIPT
+
+user=`stat -f "%Su" /dev/console`
+user_home="/Users/$user"
+plist="${user_home}/Library/Containers/com.microsoft.Outlook/Data/Library/Preferences/com.microsoft.Outlook.plist"
+
+
+
+outlook_interface=`sudo -u $user defaults read com.microsoft.Outlook IsRunningNewOutlook`
+
+if [ "$outlook_interface" == "1" ] ; then
+	echo "<result>New Outlook</result>"
+else
+	echo "<result>Classic Outlook</result>"
+fi
